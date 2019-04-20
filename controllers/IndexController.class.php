4ac51cb17388df5abeed7278f179fe8ab24172b0
Fred BadLieutenant <fredoudjoudi@gmail.com>
2019-04-21 01:13:50 +0200

    add vendor dir

--- controllers/IndexController.class.php
+++ controllers/IndexController.class.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class XController extends Controller
+class IndexController extends Controller
 {
 	public function  __construct()
 	{
--- controllers/TestController.class.php
+++ controllers/TestController.class.php
@@ -5,58 +5,27 @@ class TestController extends Controller
 {
 	public function  __construct()
 	{
-		// $alb = Db::getInstance()->select('SELECT `id_album` FROM  `album` ');
-
-		// $ignore = array(597, 524, 151, 119, 105, 405, 575, 319, 55, 36, 206 );
-		// foreach($alb as $a)
-		// {
-		// 	if(!in_array($a['id_album'], $ignore))
-		// 	{
-		// 	    $res = Db::getInstance()->getValue('select `meta_year` from song where id_album = '.$a['id_album'].' ');
-
-		// 		$album = new Album($a['id_album']);
-		// 		$album->meta_year = $res;
-		// 		$album->update();
-		// 		// d($album);
-		// 	}
-		// }
-
-		// die('here');
-	}
-
-	public function display()
+        }
+        
+        public function display()
 	{
-		$data['albums'] =   Album::getAlbums();
-		$this->renderView( $data );
 	}
 
 
 	public function test()
 	{
-		$id_album = $this->request->getValue('id_album');
-		$data['album'] = new Album( $id_album );
-		$this->renderView( $data );
-
+	    $id_song = $this->request->getValue('id_song');
+            $song = new Song($id_song);
+            // d($song);
+            $audioinfo = new AudioInfo();
+            d($audioinfo->Info($song->path));
 	}
 
 	public function ajax()
-	{
-		$albums = Album::getAlbums();
-		foreach( $albums as $a ){
-			$album = new Album( $a['id_album']);
-			$data['albums'][] = $album;
-		}
-		die(json_encode($data));
-	}
-
+        {
+        }
 	public function detail()
 	{
-		$id_album = $this->request->getValue('id_album');
-		$album = new Album( $id_album );
-
-		$data['album'] = $album;
-
-		die(json_encode($data));
 	}
 
 }
