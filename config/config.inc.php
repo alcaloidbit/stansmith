9db2666ccaf1e5f80a9d7dda24f82f3cf8878125
Fred BadLieutenant <fredoudjoudi@gmail.com>
2019-04-21 01:41:13 +0200

    edit config to use composer autoload

--- config/config.inc.php
+++ config/config.inc.php
@@ -4,6 +4,7 @@
 
 include dirname(__FILE__).'/../models/autoload.class.php';
 include dirname(__FILE__).'/settings.inc.php';
+include dirname(__FILE__).'/../vendor/autoload.php';
 
 // include getID3() library (can be in a different directory if full path is specified)
 // include dirname(__FILE__).'/../getID3-master/getid3/getid3.php';
